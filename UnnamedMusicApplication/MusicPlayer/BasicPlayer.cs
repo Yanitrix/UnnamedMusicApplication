d59a62017266dd23f567b87e0bde1c21bc8192a7
Yanitrix <35900604+Yanitrix@users.noreply.github.com>
2020-02-22 21:16:37 +0100

    added interface for music player and basic implementation

---
+++ UnnamedMusicApplication/MusicPlayer/BasicPlayer.cs
@@ -0,0 +1,61 @@
+﻿using NetCoreAudio;
+using NetCoreAudio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnnamedMusicApplication.MusicPlayer
+{
+    class BasicPlayer : IMusicPlayer
+    {
+        private readonly IPlayer player;
+
+        public event EventHandler OnPlayingFinished;
+
+        public BasicPlayer()
+        {
+            player = new Player();
+
+            player.PlaybackFinished += OnPlaybackFinished;
+        }
+
+        private void OnPlaybackFinished(object sender, EventArgs e)
+        {
+            OnPlayingFinished?.Invoke(this, e);
+        }
+
+        public bool Playing => player.Playing;
+        public bool Paused => player.Paused;
+
+        public Task Pause()
+        {
+            return player.Pause();
+        }
+
+        public Task Play(string path)
+        {
+            return player.Play(path);
+        }
+
+        public Task Resume()
+        {
+            return player.Resume();
+        }
+
+        public Task Stop()
+        {
+            return player.Stop();
+        }
+
+        public Task SkipBackward(int seconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SkipForward(int seconds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnnamedMusicApplication/MusicPlayer/IMusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnnamedMusicApplication.MusicPlayer
+{
+    interface IMusicPlayer
+    {
+        Task Play(string path);
+
+        Task Stop();
+
+        Task Resume();
+
+        Task Pause();
+
+        bool Playing { get; }
+
+        bool Paused { get; }
+
+        event EventHandler OnPlayingFinished;
+
+        Task SkipForward(int seconds);
+
+        Task SkipBackward(int seconds);
+    }
+}
