a40007644e2ec82581fffcb14e2ca08e70c50de8
Yanitrix <35900604+Yanitrix@users.noreply.github.com>
2020-03-10 18:37:38 +0100

    added docs do Queue

--- UnnamedMusicApplication/DataModel/Queue/IQueue.cs
+++ UnnamedMusicApplication/DataModel/Queue/IQueue.cs
@@ -12,14 +12,34 @@ namespace UnnamedMusicApplication.DataModel.Queue
 
         T Previous { get; }
 
+
+        /// <summary>
+        /// Inserts an element after <code>Current</code> and keeps the rest of the queue the same.
+        /// </summary>
+        /// <param name="obj"></param>
         void Insert(T obj);
 
+        /// <summary>
+        /// Inserts a collection of elements after <code>Current</code> and keeps the rest of the queue the same.
+        /// </summary>
+        /// <param name="objs"></param>
         void Insert(IEnumerable<T> objs);
 
+        /// <summary>
+        /// Clears everything after <code>Current</code> and inserts a collection of elements.
+        /// </summary>
+        /// <param name="objs"></param>
         void Jump(IEnumerable<T> objs);
 
+        /// <summary>
+        /// Adds element to the end of the queue.
+        /// </summary>
+        /// <param name="obj"></param>
         void Add(T obj);
 
+        /// <summary>
+        /// Removes all elements from the queue.
+        /// </summary>
         void Clear();
 
     }
