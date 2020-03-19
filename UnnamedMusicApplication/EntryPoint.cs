4511cfc6efcdc233996559c39259b8d50f1a0838
Yanitrix <35900604+Yanitrix@users.noreply.github.com>
2020-03-19 21:15:04 +0100

    Entry point, because the program had some issues with itself

---
+++ UnnamedMusicApplication/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnnamedMusicApplication.NewFolder
+{
+    class EntryPoint
+    {
+        public static void Main(String[] args)
+        {
+
+        }
+    }
+}
