ae59c1d1f1742c619ccc57827b88b4953291213c
yanitrix <dominiix.2001@gmail.com>
2021-02-16 22:37:54 +0100

    Made ITagProvider immutable.
    Added custom exception and threw it in the implementation.
    Closes #6

---
+++ src/Domain/Exceptions/UnsupportedFileFormatException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class UnsupportedFileFormatException : Exception
+    {
+        public UnsupportedFileFormatException()
+        {
+        }
+
+        public UnsupportedFileFormatException(string message) : base(message)
+        {
+        }
+
+        public UnsupportedFileFormatException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- src/Domain/Tags/ITagProvider.cs
+++ src/Domain/Tags/ITagProvider.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Domain.Tags
 {
     public interface ITagProvider
     {
-        void Set(String path);
-
-        void Clear();
-
-        Dictionary<string, string> Tags { get; }
+        IReadOnlyDictionary<String, String> ReadTags(String filePath);
     }
 }
