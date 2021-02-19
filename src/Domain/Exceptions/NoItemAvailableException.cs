38e4a2ed77299ac9ec2f9014b615a7b465994a72
yanitrix <dominiix.2001@gmail.com>
2021-02-19 12:45:54 +0100

    Renamed the exception.

---
+++ src/Domain/Exceptions/NoItemAvailableException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class NoItemAvailableException : Exception
+    {
+        public NoItemAvailableException()
+        {
+        }
+
+        public NoItemAvailableException(string message) : base(message)
+        {
+        }
+
+        public NoItemAvailableException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
