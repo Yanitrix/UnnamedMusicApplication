070ee791b6dfc241dc16340d85b650b0f176d281
yanitrix <dominiix.2001@gmail.com>
2021-02-24 13:56:07 +0100

    service for exporting/importing playlists

---
+++ src/Infrastructure/Services/Playlist extraction/IPlaylistExportService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+
+namespace Infrastructure.Services
+{
+    //will export csv files to given output directory
+    public interface IPlaylistExportService
+    {
+        public void ExportPlaylists(String outputDirectory, params String[] names);
+
+        public void ExportPlaylists(String outputDirectory, params Playlist[] playlists);
+    }
+}
