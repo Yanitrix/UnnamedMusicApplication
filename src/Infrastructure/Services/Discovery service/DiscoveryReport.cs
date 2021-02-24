0cc7a32c2c06ce85d863f1b8ce1837e4fbf856e1
yanitrix <dominiix.2001@gmail.com>
2021-02-24 13:55:46 +0100

    Service for discovering files

---
+++ src/Infrastructure/Services/Discovery service/DiscoveryReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class DiscoveryReport
+    {
+        public int ArtistCount { get; set; }
+
+        public int AlbumCount { get; set; }
+
+        public int SongCount { get; set; }
+
+        public long TotalBytes { get; set; }
+    }
+}
---
+++ src/Infrastructure/Services/Discovery service/IDiscoveryService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    /// <summary>
+    /// Discovers all songs in given directory and inserts them into database.
+    /// </summary>
+    public interface IDiscoveryService
+    {
+        public DiscoveryReport Run(String[] rootDirectories, ComparisonMode comparisonMode);
+    }
+}
