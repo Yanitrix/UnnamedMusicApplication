67ea9c09e882096c1caedbdd1b313ac57fb4faaf
yanitrix <dominiix.2001@gmail.com>
2021-02-24 13:54:49 +0100

    Data access interfaces

---
+++ src/Domain/Data access/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.DataAccess.Abstract
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        //querying by substring?
+        //yup, should be queried by substring but limited in some way
+        public IEnumerable<T> GetByName(String name);
+
+        public T GetById(long id);
+
+        public void Add(T entity);
+
+        //TODO update/delete entities
+        //is deleting/updating really possible? I think only when rediscovering of files happens but then only adding should be used (we don't know what to delete/update since everything has changed)
+        //or should discovering not be run everytime? but that would enforce me to use some watches, etc.
+        //removing Update/Delete for now, useful only in IPLaylistRepo
+    }
+}
---
+++ src/Domain/Data access/IPlaylistRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.DataAccess.Abstract;
+using Domain.Entities;
+using System.Collections.Generic;
+
+namespace Domain.DataAccess
+{
+    //TODO playlist should be updated when the files are rediscovered
+    public interface IPlaylistRepository : IRepository<Playlist>
+    {
+        public IEnumerable<Playlist> All();
+
+        //and here full crud can be performed
+        public void Update(Playlist entity);
+
+        public void Delete(Playlist entity);
+    }
+}
