056dab32eb3f5f1555d0c16a1532241437cf1473
yanitrix <dominiix.2001@gmail.com>
2021-02-26 15:42:57 +0100

    Added an interface to save/read it

---
+++ src/Infrastructure/Services/Discovery service/Discovery mode/ICurrentDiscoveryModeService.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Services
+{
+    public interface ICurrentDiscoveryModeService
+    {
+        //get will get it from some file
+        //set will write to that file
+        DiscoveryMode CurrentDiscoveryMode { get; set; }
+    }
+}
