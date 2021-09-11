3abdcdfa9ab8e61becf253e3c98258be8970fa47
Yanitrix <dominiix.2001@gmail.com>
2021-09-11 23:08:23 +0200

    Revert "removed old stuff"
    This reverts commit daf1e835d2e61239b2d6fc0e885d1be0c8bce41a.
    
    # Conflicts:
    #	src/Domain/Queue/SongQueue.cs

--- src/Domain/Queue/SongQueue.cs
+++ src/Domain/Queue/SongQueue.cs
@@ -1,73 +1,64 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Domain.Entities;
+﻿using Domain.Entities;
+using System.Collections.Generic;
 
 namespace Domain.Queue
 {
     public class SongQueue : ISongQueue
     {
+        private readonly List<int> history; //the history can be used as a singleton list
         private readonly List<Song> songs;
-        private PlayingMode mode;
+        private int currentIndex = -1;
         
-        public SongQueue(List<Song> songs)
+        private ISongQueue inner;
+
+        public SongQueue()
         {
-            this.songs = songs; 
+            inner = new LinearQueue(songs, currentIndex);
         }
 
-        public bool HasNext => mode.HasNext;
-        
-        public Song Next => songs[mode.Next];
+        private bool shuffle;
 
-        public bool HasPrevious => mode.HasPrevious;
+        public Song Next => inner.Next;
 
-        public Song Previous => songs[mode.Previous];
+        public Song Previous => inner.Previous;
 
-        public bool Shuffle { get; set; }
+        public bool HasNext => inner.HasNext;
 
-        public void Insert(Song song)
+        public bool HasPrevious => inner.HasPrevious;
+
+        public bool Shuffle
         {
-            songs.Insert(mode.Current, song);
-            mode.Count++;
+            get => shuffle;
+
+            set
+            {
+                if (value == shuffle)
+                    return;
+                if (value)
+                {
+                    inner = new ShuffleQueue(this.songs, currentIndex);
+                    shuffle = true;
+                }
+                else
+                {
+                    inner = new LinearQueue(this.songs, currentIndex);
+                    shuffle = false;
+                }
+            }
         }
 
-        public void Insert(IEnumerable<Song> songs)
-        {
-            var enumerable = songs as Song[] ?? songs.ToArray();
-            var count = enumerable.Length + this.songs.Count;
-            this.songs.AddRange(enumerable);
-            mode.Count = count;
-        }
+        public void Add(Song song) => inner.Add(song);
 
-        public void Replace(IEnumerable<Song> songs)
-        {
-            var count = (mode.Current, this.songs.Count - 1).CountBetween();
-            
-            this.songs.RemoveRange(mode.Current, count);
-            var enumerable = songs as Song[] ?? songs.ToArray();
-            var offset = enumerable.Length - count;
-            mode.Count += offset;
-            this.songs.AddRange(enumerable);
-        }
+        public void Clear() => inner.Clear();
 
-        public void Add(Song song)
-        {
-            songs.Add(song);
-            mode.Count++;
-        }
+        public void Insert(Song song) => inner.Insert(song);
 
-        public void Clear()
-        {
-            songs.Clear();
-        }
+        public void Insert(IEnumerable<Song> songs) => inner.Insert(songs);
 
-        public void Set(IEnumerable<Song> songs)
-        {
-            this.songs.Clear();
-            var enumerable = songs.ToList();
-            var count = enumerable.Count;
-            this.songs.AddRange(enumerable);
-        }
+        public void Replace(IEnumerable<Song> songs) => inner.Replace(songs);
 
-        public void Reset() => mode.Reset();
+        public void Reset() => inner.Reset();
+
+        public void Set(IEnumerable<Song> songs) => inner.Set(songs);
     }
 }
