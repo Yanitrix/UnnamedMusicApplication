2ea100a23b811c82e084dd6adb9d250d4853f929
Yanitrix <35900604+Yanitrix@users.noreply.github.com>
2021-09-17 23:24:18 +0200

    Proper queue implementation with shuffle (for now) (#21)
     1. Queue with shuffle mode
     2. Tests

---
+++ src/Domain/Queue/RangeExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Queue
+{
+    public static class RangeExtensions
+    {
+        /// <summary>
+        /// Both inclusive.
+        /// </summary>
+        public static int CountBetween(this (int beginIndex, int lastIndex) range)
+        {
+            return range.lastIndex - range.beginIndex + 1;
+        }
+
+        /// <summary>
+        /// Last inclusive.
+        /// </summary>
+        public static int LastIndex(this (int beginIndex, int count) range)
+        {
+            return range.beginIndex + range.count - 1;
+        }
+    }
+}
--- tests/Domain.Tests/QueueTestsWithShuffle.cs
+++ tests/Domain.Tests/QueueTestsWithShuffle.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using Domain.Entities;
 using Domain.Queue;
+using FluentAssertions;
 using Xunit;
 
 namespace Domain.Tests
 {
     public class QueueTestsWithShuffle
     {
-        private List<Song> TestData(int count = 10)
+        private static List<Song> TestData(int count)
         {
             return Enumerable.Range(1, count).Select(x => new Song {Id = x}).ToList();
         }
@@ -18,60 +19,120 @@ namespace Domain.Tests
         public void ShuffleFromStart_OrderShouldBeRandom()
         {
             ISongQueue queue = new SongQueue();
-            var generated = TestData(18);
-            queue.Set(generated);
+            var initial = TestData(18);
+            queue.Set(initial);
+            
+            //act
+            var traversed = new List<Song>();
             queue.Shuffle = true;
-            
-            var songs = new List<Song>();
             while(queue.HasNext)
-                songs.Add(queue.Next);
-
-            var actual = songs.Select(x => x.Id);
-            var expected = generated.Select(x => x.Id);
+                traversed.Add(queue.Next);
             
-            Assert.Equal(actual.Count(), expected.Count());
-            Assert.NotEqual(expected, actual);
-            Assert.Equal(expected, actual.OrderBy(x => x));
+            //assert
+            traversed.Count.Should().Be(initial.Count);
+            traversed.Should().NotEqual(initial);
+            traversed.OrderBy(x => x.Id).Should().Equal(initial);
         }
 
         [Fact]
-        public void ShuffleInTheMiddle_HasNoPrevious()
+        public void ShuffleInTheMiddle_HasNoPrevious_FirstElementIsWhereShuffleStarted()
         {
             ISongQueue queue = new SongQueue();
-            queue.Set(TestData(10));
+            var initial = TestData(10);
+            queue.Set(initial);
             
-            //skip three times
+            //act
             _ = queue.Next;
             _ = queue.Next;
-            _ = queue.Next;
-
+            var expected = queue.Next;
             queue.Shuffle = true;
             
+            //asset
             Assert.False(queue.HasPrevious);
             Assert.True(queue.HasNext);
+            //started shuffle on third index so it should be the first
+            queue.Content[0].Should().Be(expected);
         }
 
         [Fact]
-        public void ShuffleInTheMiddle_RemainingIndicesAreRandom()
+        public void ShuffleInTheMiddle_WholeQueueIsRandom()
         {
             ISongQueue queue = new SongQueue();
-            queue.Set(TestData(26));
+            var initial = TestData(26);
+            queue.Set(initial);
 
+            //act
+            var actual = new List<Song>();
             for (var i = 0; i < 4; i++)
                 _ = queue.Next;
-
-            queue.Shuffle = true;
-
-            var songs = new List<Song>();
-            while(queue.HasNext)
-                songs.Add(queue.Next);
-
-            var actual = songs.Select(x => x.Id);
-            var expected = Enumerable.Range(5, 22).Select(Convert.ToInt64); //from 5 to 26 because first 4 were used
             
-            Assert.Equal(expected.Count(), actual.Count());
-            Assert.NotEqual(expected, actual);
-            Assert.Equal(expected, actual.OrderBy(x => x));
+            queue.Shuffle = true;
+            while(queue.HasNext)
+                actual.Add(queue.Next);
+
+            //assert
+            Assert.Equal(actual.Count, initial.Count);
+            actual
+                .Should()
+                .NotEqual(initial);
+            actual.OrderBy(x => x.Id)
+                .Should()
+                .Equal(initial);
+        }
+
+        [Fact]
+        public void Shuffle_TraverseWhole_SequenceIsTheSameAsContentProperty()
+        {
+            //arrange
+            ISongQueue queue = new SongQueue();
+            var initial = TestData(113);
+            queue.Set(initial);
+            queue.Shuffle = true;
+            var traversed = new List<Song>();
+            IList<Song> content;
+
+            //act
+            content = queue.Content.ToList();
+            while(queue.HasNext)
+                traversed.Add(queue.Next);
+
+            //assert
+            //also check if the first original is the first on the list
+            content.Should().Equal(traversed);
+            content.Should().NotEqual(initial);
+            content.Count.Should().Be(initial.Count);
+            content.Count.Should().Be(traversed.Count);
+            content
+                .OrderBy(x => x.Id)
+                .Should()
+                .Equal(initial);
+            traversed
+                .OrderBy(x => x.Id)
+                .Should()
+                .Equal(initial);
+            traversed[0].Should().Be(initial.First());
+            content[0].Should().Be(initial.First());
+        }
+
+        [Fact]
+        public void TurnShuffleOn_ThenOff_CurrentSongIsTheSameAsTheOneThatShuffleWasTurnedOffOn()
+        {
+            //arrange
+            ISongQueue queue = new SongQueue();
+            var initial = TestData(1200);
+            queue.Set(initial);
+            queue.Shuffle = true;
+            
+            //act
+            for (int i = 0; i < 16; i++) //skip 16 in total and take the 17th
+                _ = queue.Next;
+            var expeted = queue.Next;
+            queue.Shuffle = false;
+
+            //assert
+            queue.Content[queue.CurrentIndex].Should().Be(expeted);
+            queue.Content.Count.Should().Be(initial.Count);
+            queue.Content.Should().Equal(initial);
         }
     }
 }
